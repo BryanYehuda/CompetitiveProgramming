efedf9f1ecc460cff38de1ddc77e9f9ca27e5b12
BryanYehuda <bryanyehuda@gmail.com>
2023-05-03 10:52:53 +0700

    Adding more files

--- SPOJ/ACC/SNOWMAN.cpp
+++ SPOJ/ACC/SNOWMAN.cpp
@@ -10,7 +10,7 @@ int main() {
     scanf("%d",&cases);
     while(cases--) {
          scanf("%d",&vol);
-         radius = pow (16.0*(double)vol/(43.0*PI), 1.0/3.0)
+         radius = pow (16.0*(double)vol/(43.0*PI), 1.0/3.0);
          height = (9.0*radius)/2.0;
          printf("%d\n",(int)height);        
          }
